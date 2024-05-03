23cff654f8698f1ab22426f3b72f50bfd959e00f
Rahmat Mahardika <luxyferprime@gmail.com>
2024-05-04 00:33:08 +0700

    update

--- rc4.js
+++ rc4.js
@@ -1,14 +1,14 @@
-function RC4(key) {
+function RC4(key, sBoxLength) {
     let s = [];
     let k = [];
     let keyLength = key.length;
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < sBoxLength; i++) {
         s[i] = i;
         k[i] = key.charCodeAt(i % keyLength);
     }
     let j = 0;
-    for (let i = 0; i < 256; i++) {
-        j = (j + s[i] + k[i]) % 256;
+    for (let i = 0; i < sBoxLength; i++) {
+        j = (j + s[i] + k[i]) % sBoxLength;
         let temp = s[i];
         s[i] = s[j];
         s[j] = temp;
@@ -19,12 +19,12 @@ function RC4(key) {
         let j = 0;
         let cipher = '';
         for (let x = 0; x < data.length; x++) {
-            i = (i + 1) % 256;
-            j = (j + s[i]) % 256;
+            i = (i + 1) % sBoxLength;
+            j = (j + s[i]) % sBoxLength;
             let temp = s[i];
             s[i] = s[j];
             s[j] = temp;
-            let keyIndex = (s[i] + s[j]) % 256;
+            let keyIndex = (s[i] + s[j]) % sBoxLength;
             let xor = s[keyIndex];
             cipher += String.fromCharCode(xor ^ data.charCodeAt(x));
         }
--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.getElementById('encryptForm').addEventListener('submit', function(event
     event.preventDefault();
     let plainText = document.getElementById('plainText').value;
     let key = document.getElementById('key').value;
+    let sBoxLength = parseInt(document.getElementById('sBoxLength').value); // Menambahkan baris untuk mengambil panjang s-box
     console.log("Plain Text:", plainText);
     console.log("Key:", key);
-    let encryptedText = RC4(key)(plainText);
+    let rc4Instance = RC4(key, sBoxLength); // Membuat instance objek RC4 dengan panjang s-box yang ditentukan
+    let encryptedText = rc4Instance(plainText);
     console.log("Encrypted Text:", encryptedText);
     document.getElementById('encryptedText').innerText = "Encrypted Text: " + encryptedText;
 });
@@ -13,9 +15,11 @@ document.getElementById('decryptForm').addEventListener('submit', function(event
     event.preventDefault();
     let cipherText = document.getElementById('cipherText').value;
     let decryptionKey = document.getElementById('decryptionKey').value;
+    let sBoxLength = parseInt(document.getElementById('sBoxLength').value); // Menambahkan baris untuk mengambil panjang s-box
     console.log("Cipher Text:", cipherText);
     console.log("Decryption Key:", decryptionKey);
-    let decryptedText = RC4(decryptionKey)(cipherText);
+    let rc4Instance = RC4(decryptionKey, sBoxLength); // Membuat instance objek RC4 dengan panjang s-box yang ditentukan
+    let decryptedText = rc4Instance(cipherText);
     console.log("Decrypted Text:", decryptedText);
     document.getElementById('decryptedText').innerText = "Decrypted Text: " + decryptedText;
 });
