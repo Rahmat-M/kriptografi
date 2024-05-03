59aac8db89b9bd80db297335c9fb749d165814aa
Rahmat Mahardika <luxyferprime@gmail.com>
2024-05-04 00:37:26 +0700

    update

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.getElementById('encryptForm').addEventListener('submit', function(event
     event.preventDefault();
     let plainText = document.getElementById('plainText').value;
     let key = document.getElementById('key').value;
-    let sBoxLength = parseInt(document.getElementById('sBoxLength').value); // Menambahkan baris untuk mengambil panjang s-box
+    let sBoxLength = parseInt(document.getElementById('sBoxLength').value); 
     console.log("Plain Text:", plainText);
     console.log("Key:", key);
-    let rc4Instance = RC4(key, sBoxLength); // Membuat instance objek RC4 dengan panjang s-box yang ditentukan
+    let rc4Instance = RC4(key, sBoxLength); 
     let encryptedText = rc4Instance(plainText);
     console.log("Encrypted Text:", encryptedText);
     document.getElementById('encryptedText').innerText = "Encrypted Text: " + encryptedText;
@@ -15,10 +15,10 @@ document.getElementById('decryptForm').addEventListener('submit', function(event
     event.preventDefault();
     let cipherText = document.getElementById('cipherText').value;
     let decryptionKey = document.getElementById('decryptionKey').value;
-    let sBoxLength = parseInt(document.getElementById('sBoxLength').value); // Menambahkan baris untuk mengambil panjang s-box
+    let sBoxLength = parseInt(document.getElementById('sBoxLength').value); 
     console.log("Cipher Text:", cipherText);
     console.log("Decryption Key:", decryptionKey);
-    let rc4Instance = RC4(decryptionKey, sBoxLength); // Membuat instance objek RC4 dengan panjang s-box yang ditentukan
+    let rc4Instance = RC4(decryptionKey, sBoxLength); 
     let decryptedText = rc4Instance(cipherText);
     console.log("Decrypted Text:", decryptedText);
     document.getElementById('decryptedText').innerText = "Decrypted Text: " + decryptedText;
